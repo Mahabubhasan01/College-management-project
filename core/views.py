fd6fd5fd54177cbacd034c7f767549fd5999aa0c
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-07-08 12:12:29 +0600

    dashboard add

--- core/views.py
+++ core/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm, UserChangeForm, UserCreationForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 # Create your views here.
+
+
 def dashboard(request):
-    return render(request,'dashboard.html')
+    return render(request, 'dashboard.html')
+
+
 def user_signup(request):
-    return render(request,'signup.html')
+    return render(request, 'signup.html')
+
+
 def user_login(request):
-    return render(request,'login.html')
+    fm = AuthenticationForm(request=request, data=request.POST)
+    if fm.is_valid():
+        username = fm.cleaned_data['username']
+        password = fm.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login Successfully!!!')
+            return HttpResponseRedirect('/profile/')
+    else:
+        fm = AuthenticationForm()
+    return render(request, 'login.html', {'form': fm})
